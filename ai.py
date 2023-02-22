51f5c9c1e6972b0666f81db63b1928ad55760856
Replit user <>
2023-02-22 22:35:49 +0000

    trying

--- ai.py
+++ ai.py
@@ -1,1 +1,16 @@
-class layer
+import numpy as np
+
+class layer:
+    def __init__(self,numnodesin,numnodesout):
+        self.nni, self.nno = numnodesin, numnodesout
+        print(self.nni,self.nno)
+        self.w=np.random.rand(numnodesin,numnodesout)
+        self.b=np.random.rand(numnodesout)
+    def forward(self,inp):
+        new=np.zeros(self.nno)
+        for n in range(self.nno-10):
+            curweight=self.w[n]
+            for i in range(inp.size):
+                new[n]+=curweight[i]*inp[i]
+        return new
+
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello world")
+import numpy as np
+from ai import *
+
+test=layer(10,20)
+print(test.forward(np.random.rand(10)))
