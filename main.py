7f9e3460e4f8531c88fedadaeb250252b982bb87
Magician357 <65839659+Magician357@users.noreply.github.com>
2023-03-06 08:10:43 -0800

    Removing old code

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import numpy as np
-from ai import *
-from smalldataset import data
 
-test=trainer(data,20,[2,3,2])
-cur=test.best.forward(data[0][0])
-print(cur)
-best=test.best
-for n in range(100):
-    test.train()
-    cur=tuple(test.best.forward(data[0][0]))
-    print(disttuple(cur,data[0][1]))
-    if disttuple(cur,data[0][1]) < disttuple(best.forward(data[0][0]),data[0][1]):
-        best=test.best
-
-print("best:")
-print(disttuple(best.forward(data[0][0]),data[0][1]))
